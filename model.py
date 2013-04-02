6864e7609e0e726f175ba73543c9c30d80e1af06
Hackbright Student <user@mini-004.(none)>
2013-04-01 17:44:47 -0700

    have run seed.py to seed db with ratings, users, and movie data

--- model.py
+++ model.py
@@ -1,13 +1,66 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+
+ENGINE = None
+Session = None
 
 Base = declarative_base()
 
 ### Class declarations go here
+class User (Base):
+	__tablename__ = "users"
 
+	id = Column(Integer, primary_key = True)
+	email = Column(String(64), nullable = True)
+	password = Column(String(64), nullable = True)
+	age = Column(Integer, nullable = True)
+	zipcode = Column(String(15), nullable = True)
+
+	# don't need init function because we're inheriting from Base and not doing any calculations here anyway
+	def __init__(self, age, zipcode, email = None, password = None):
+		self.email = email
+		self.password = password
+		self.age = age
+		self.zipcode = zipcode
+
+class Movies (Base):
+	__tablename__ = "movies"
+
+	id = Column(Integer, primary_key = True)
+	name = Column(String(128))
+	released_at = Column(String(128), nullable = True)
+	imdb_url = Column(String(128), nullable = True)
+
+	def __init__(self, name, released_at = None, imdb_url = None):
+		self.name = name
+		self.released_at = released_at
+		self.imdb_url = imdb_url
+  
+class Ratings (Base):
+	__tablename__ = "ratings"
+
+	id = Column(Integer, primary_key = True)
+	movie_id = Column(Integer)
+	user_id = Column(Integer)
+	rating = Column(Integer)
+
+	def __init__(self, movie_id, user_id, rating):
+		self.movie_id = movie_id
+		self.user_id = user_id
+		self.rating = rating
 
 ### End class declarations
+def connect():
+	global ENGINE
+	global Session
+
+	ENGINE = create_engine("sqlite:///ratings.db", echo = False)
+	Session = sessionmaker(bind = ENGINE)
+
+	return Session() # return an instance of one connection to the session
+
 
 def main():
     """In case we need this for something"""
--- seed.py
+++ seed.py
@@ -3,19 +3,50 @@ import csv
 
 def load_users(session):
     # use u.user
-    pass
+    with open('seed_data/u.user') as csvfile:   
+        # dialect = csv.Sniffer().sniff(csvfile.read(1024))
+        # csvfile.seek(0)
+        userlist = csv.reader(csvfile, delimiter = "|")
+        for row in userlist:
+            new_user = model.User(age=row[1], zipcode=row[4])
+            new_user.id = row[0]
+            session.add(new_user)
+
 
 def load_movies(session):
     # use u.item
-    pass
+    count = 0
+    with open('seed_data/u.item') as csvfile:
+        itemlist = csv.reader(csvfile, delimiter = "|")
+        for row in itemlist:
+            row[1] = row[1].decode("latin-1") # change names from latin-1 encoding to unicode strings for sqlite3 db
+            new_movie = model.Movies(name=row[1], released_at=row[2], imdb_url=row[4])
+            new_movie.id = row[0]
+            session.add(new_movie)
+            count += 1
+            print "Movie #", count
+
 
 def load_ratings(session):
     # use u.data
-    pass
+    count = 0
+    with open('seed_data/u.data') as csvfile:
+        dialect = csv.Sniffer().sniff(csvfile.read(1024))
+        csvfile.seek(0)
+        itemlist = csv.reader(csvfile, dialect)
+        for row in itemlist:
+            new_rating = model.Ratings(movie_id=row[1], user_id=row[0], rating=row[2])
+            session.add(new_rating)
+            count += 1
+            print "Rating #", count
 
 def main(session):
     # You'll call each of the load_* functions with the session as an argument
-    pass
+    load_users(session)
+    load_movies(session)
+    load_ratings(session)
+    
+    session.commit() # much much faster to commit all the new rows all at once
 
 if __name__ == "__main__":
     s= model.connect()
